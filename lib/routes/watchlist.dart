3f6e17f97b0e3d4441e2f03c511bd3312971becd
Krzysztof Piątkowski <ra.krzysztof.piatkowski@gmail.com>
2021-05-20 23:37:14 +0200

    Started development on watchlist

--- lib/routes/watchlist.dart
+++ lib/routes/watchlist.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:xlo_auction_app/authentication/authentication.dart';
 
-class Watchlist extends StatelessWidget {
+class Watchlist extends StatefulWidget {
+  const Watchlist({Key key}) : super(key: key);
+
+  @override
+  _WatchlistState createState() => _WatchlistState();
+}
+
+class _WatchlistState extends State<Watchlist> {
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthenticationService>(context);
-
-    return Container(
-        child: CupertinoButton(
-          onPressed: () {
-          auth.signOut();
-        },
-          child: const Text(
-          'watchlist',
-          style: TextStyle(color: CupertinoColors.white),
-          ),
-      color: CupertinoColors.activeBlue,
-    ));
+    return CupertinoTabView(
+      builder: (context){
+        return CupertinoPageScaffold(
+            child: SafeArea(
+              child: CupertinoScrollbar(
+                child: CustomScrollView(
+                  slivers: [
+                    CupertinoSliverNavigationBar(
+                      largeTitle: Text('Watchlist'),
+                    )
+                  ],
+                ),
+              ),
+            )
+        );
+      },
+    );
   }
 }
